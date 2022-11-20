c320c8d1d77dcb3be214b53b5a3c811ed0072041
uncleFrans <frans261087@gmail.com>
2022-11-20 20:18:42 +0700

    Delete layout component Implement template for each pages

--- components/ui/layout.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-import styles from '../../styles/components/ui/layout.module.scss';
-export const Layout = ({ children }) => {
-  return <div className={styles.container}>{children}</div>;
-};
-
-Layout.propTypes = {
-  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired
-};
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,7 @@
-import Card from '../components/ui/card';
-import RegisterLogin from '../components/form/registerLogin';
-import styles from '../styles/Home.module.css';
+import LoginTemplate from '../components/template/loginTemplate';
 
 const Home = () => {
-  return (
-    <div className="container">
-      <main className={styles.main}>
-        <Card>
-          <RegisterLogin />
-        </Card>
-      </main>
-    </div>
-  );
+  return <LoginTemplate />;
 };
 
 export default Home;
